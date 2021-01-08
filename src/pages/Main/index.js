e3c27d98b760df96079fe36b81afd91236147840
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-01-08 09:58:21 -0300

    navigation + starred list

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Keyboard, ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -19,6 +20,16 @@ import {
 } from './styles';
 
 class Main extends Component {
+  static navigationOptions = {
+    title: 'Usuários',
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   state = {
     newUser: '',
     users: [],
@@ -64,6 +75,12 @@ class Main extends Component {
     Keyboard.dismiss();
   };
 
+  handleNavigate = (user) => {
+    const { navigation } = this.props;
+
+    navigation.navigate('User', { user });
+  };
+
   render() {
     const { users, newUser, loading } = this.state;
 
@@ -97,7 +114,7 @@ class Main extends Component {
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
 
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
                 <ProfileButtonText>Ver Perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -109,7 +126,3 @@ class Main extends Component {
 }
 
 export default Main;
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
